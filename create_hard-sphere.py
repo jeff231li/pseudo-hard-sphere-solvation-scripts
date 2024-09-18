ff7afb7701205686bb4083d34a528948963b8ec8
jset231 <pea231@gmail.com>
2024-09-17 17:30:53 -0700

    add files

---
+++ create_hard-sphere.py
@@ -0,0 +1,105 @@
+import openmm
+import openmm.app as app
+import openmm.unit as unit
+
+from hard_sphere import make_guest_wca_repulsive
+
+# User Option
+# ----------------------------------------------------------------------- #
+guest_resname = "DM1"
+host_resname = "CB8"
+solvent_resname = "HOH"
+
+hard_sphere_sigma = 3.0 * unit.angstrom
+hard_sphere_epsilon = 0.1 * unit.kilocalorie_per_mole
+hard_sphere_radius = 5.0 * unit.angstrom
+
+wca_repulsive = 50
+wca_attractive = 49
+
+# Load Amber Files
+# ----------------------------------------------------------------------- #
+prmtop = app.AmberPrmtopFile("structures/CB8-Guest-sol.prmtop")
+inpcrd = app.AmberInpcrdFile("structures/CB8-Guest-sol.rst7")
+
+# Get atom indices of different components in system
+# ----------------------------------------------------------------------- #
+guest = [
+    atom.index for atom in prmtop.topology.atoms() if atom.residue.name == guest_resname
+]
+host_com = [
+    atom.index
+    for atom in prmtop.topology.atoms()
+    if atom.residue.name == host_resname and atom.element.symbol == "C"
+]
+host = [
+    atom.index for atom in prmtop.topology.atoms() if atom.residue.name == host_resname
+]
+solvent = [
+    atom.index
+    for atom in prmtop.topology.atoms()
+    if atom.residue.name == solvent_resname
+]
+
+# Create OpenMM System
+# ----------------------------------------------------------------------- #
+system = prmtop.createSystem(
+    nonbondedMethod=app.PME,
+    nonbondedCutoff=9.0 * unit.angstrom,
+    constraints=app.HBonds,
+    rigidWater=True,
+)
+
+# Set Host mass to zero
+for atom in host:
+    system.setParticleMass(atom, 0.0 * unit.dalton)
+
+# Set Host molecule either "hard-sphere" or weakly dispersive
+# ----------------------------------------------------------------------- #
+# make_host_wca_dispersive(
+#    system,
+#    host,
+#    solvent,
+#    lambda_value=0.0,
+#    force_group=9,
+# )
+
+# Set Dummy particle to "hard-sphere"
+# ----------------------------------------------------------------------- #
+make_guest_wca_repulsive(
+    system,
+    guest,
+    host,
+    solvent,
+    particle_radius=hard_sphere_radius,
+    wca_sigma=hard_sphere_sigma,
+    wca_epsilon=hard_sphere_epsilon,
+    coeff_r=wca_repulsive,
+    coeff_a=wca_attractive,
+    force_group=10,
+)
+
+# Set Dummy particle LJ parameters to zero
+# ----------------------------------------------------------------------- #
+nonbonded = [
+    force for force in system.getForces() if isinstance(force, openmm.NonbondedForce)
+][0]
+for atom in guest:
+    charge, sigma, epsilon = nonbonded.getParticleParameters(atom)
+    nonbonded.setParticleParameters(atom, 0.0, sigma, 0.0)
+
+# Set Host charges and LJ parameters to zero for weakly dispersive host
+# ----------------------------------------------------------------------- #
+# for atom in host:
+#    charge, sigma, epsilon = nonbonded.getParticleParameters(atom)
+#    nonbonded.setParticleParameters(atom, 0.0, sigma, 0.0)
+#
+# for i_exception in range(nonbonded.getNumExceptions()):
+#    atom_i, atom_j, chargeprod, sigma, epsilon = nonbonded.getExceptionParameters(
+#        i_exception
+#    )
+#    if atom_i in host and atom_j in host:
+#        nonbonded.setExceptionParameters(i_exception, atom_i, atom_j, 0.0, sigma, 0.0)
+
+with open("system_hard-sphere.xml", "w") as file:
+    file.write(openmm.XmlSerializer.serialize(system))
---
+++ hard_sphere.py
@@ -0,0 +1,184 @@
+import openmm
+import openmm.app as app
+import openmm.unit as unit
+
+
+def make_guest_wca_repulsive(
+    system,
+    guest,
+    host,
+    solvents,
+    particle_radius=5.0 * unit.angstrom,
+    wca_sigma=3.0 * unit.angstrom,
+    wca_epsilon=1.0 * unit.kilocalorie_per_mole,
+    coeff_r=50,
+    coeff_a=49,
+    force_group=10,
+):
+    nonbonded = [
+        force
+        for force in system.getForces()
+        if isinstance(force, openmm.NonbondedForce)
+    ][0]
+
+    wca_repulsive = openmm.CustomNonbondedForce(
+        "U_repulsive;"
+        "U_repulsive = step(R_particle - r) * (U_Mie + epsilon_wall);"
+        "U_Mie = prefactor * epsilon_wall * ((sigma_wall/r_prime)^coeff_r - (sigma_wall/r_prime)^coeff_a);"
+        "prefactor = coeff_r/(coeff_r-coeff_a) * (coeff_r/coeff_a)^(coeff_r/(coeff_r-coeff_a));"
+        "r_prime = r - (R_particle - R_min);"
+        "R_min = sigma_wall * (coeff_r/coeff_a)^(1/(coeff_r-coeff_a));"
+    )
+    wca_repulsive.addGlobalParameter("R_particle", particle_radius)
+    wca_repulsive.addGlobalParameter("coeff_r", coeff_r)
+    wca_repulsive.addGlobalParameter("coeff_a", coeff_a)
+    wca_repulsive.addGlobalParameter("sigma_wall", wca_sigma)
+    wca_repulsive.addGlobalParameter("epsilon_wall", wca_epsilon)
+    wca_repulsive.addPerParticleParameter("sigma")
+    wca_repulsive.addPerParticleParameter("epsilon")
+    wca_repulsive.setNonbondedMethod(openmm.CustomNonbondedForce.CutoffPeriodic)
+    wca_repulsive.setCutoffDistance(nonbonded.getCutoffDistance())
+    wca_repulsive.setUseLongRangeCorrection(False)
+    wca_repulsive.setForceGroup(force_group)
+
+    wca_repulsive.addInteractionGroup(guest, (host + solvents))
+
+    # Set LJ parameters
+    for atom in range(nonbonded.getNumParticles()):
+        charge, sigma, epsilon = nonbonded.getParticleParameters(atom)
+        if atom in guest:
+            wca_repulsive.addParticle([wca_sigma, wca_epsilon])
+        else:
+            wca_repulsive.addParticle([sigma, epsilon])
+
+    system.addForce(wca_repulsive)
+
+    # Set LJ parameters to zero
+    for atom in guest:
+        [charge, sigma, epsilon] = nonbonded.getParticleParameters(atom)
+        nonbonded.setParticleParameters(atom, 0.0, wca_sigma, 0.0)
+
+    # Transfer Exclusion
+    for exception_index in range(nonbonded.getNumExceptions()):
+        iatom, jatom, chargeprod, sigma, epsilon = nonbonded.getExceptionParameters(
+            exception_index
+        )
+        wca_repulsive.addExclusion(iatom, jatom)
+
+
+def make_host_wca_dispersive(
+    system,
+    host_atoms,
+    solvent_atoms,
+    lambda_scale=1,
+    force_group=10,
+):
+    nonbonded = [
+        force
+        for force in system.getForces()
+        if isinstance(force, openmm.NonbondedForce)
+    ][0]
+
+    energy_expression = (
+        "U_dispersive;"
+        "U_dispersive = step(R_min - r)*(U_LJ + epsilon*(1 - lambda_dispersive)) + step(r - R_min)*(lambda_dispersive * U_LJ);"
+        "U_LJ = 4 * epsilon * x * (x - 1.0);"
+        "x = (sigma / r)^6;"
+        "R_min = sigma * 2^(1/6);"
+        "sigma = 0.5*(sigma1 + sigma2);"
+        "epsilon = sqrt(epsilon1 * epsilon2);"
+    )
+
+    wca_dispersive = openmm.CustomNonbondedForce(energy_expression)
+    wca_dispersive.addGlobalParameter("lambda_dispersive", lambda_scale)
+    wca_dispersive.addPerParticleParameter("sigma")
+    wca_dispersive.addPerParticleParameter("epsilon")
+    wca_dispersive.setNonbondedMethod(openmm.CustomNonbondedForce.CutoffPeriodic)
+    wca_dispersive.setCutoffDistance(nonbonded.getCutoffDistance())
+    wca_dispersive.setUseLongRangeCorrection(nonbonded.getUseDispersionCorrection())
+    wca_dispersive.addInteractionGroup(host_atoms, solvent_atoms)
+    wca_dispersive.setForceGroup(force_group)
+
+    # Set LJ parameters
+    for atom in range(nonbonded.getNumParticles()):
+        charge, sigma, epsilon = nonbonded.getParticleParameters(atom)
+        wca_dispersive.addParticle([sigma, epsilon])
+
+    # Transfer Exclusion
+    for exception_index in range(nonbonded.getNumExceptions()):
+        iatom, jatom, chargeprod, sigma, epsilon = nonbonded.getExceptionParameters(
+            exception_index
+        )
+        wca_dispersive.addExclusion(iatom, jatom)
+
+    # Add force to System
+    system.addForce(wca_dispersive)
+
+
+def add_alchemical_wca_repulsive(
+    system,
+    guest,
+    solvents,
+    particle_radius=5.0 * unit.angstrom,
+    wca_sigma=3.0 * unit.angstrom,
+    wca_epsilon=1.0 * unit.kilocalorie_per_mole,
+    coeff_r=50,
+    coeff_a=49,
+    softcore_alpha=0.5,
+    lambda_value=1.0,
+    force_group=10,
+):
+    nonbonded = [
+        force
+        for force in system.getForces()
+        if isinstance(force, openmm.NonbondedForce)
+    ][0]
+
+    wca_repulsive = openmm.CustomNonbondedForce(
+        "lambda_sterics * U_repulsive;"
+        "U_repulsive = step(R_particle - r) * (U_Mie + epsilon_wall);"
+        "U_Mie = prefactor * epsilon_wall * (1/repulsive - 1/dispersive);"
+        "repulsive = (dispersive)^(coeff_r/coeff_a);"
+        "dispersive = softcore_alpha*(1.0-lambda_sterics) + (r_prime/sigma_wall)^coeff_a;"
+        "prefactor = (coeff_r/(coeff_r - coeff_a)) * (coeff_r/coeff_a)^(coeff_a/(coeff_r-coeff_a));"
+        "r_prime = r - (R_particle - R_min);"
+        "R_min = sigma_wall * ((coeff_r/coeff_a)^(coeff_a/(coeff_r-coeff_a)) - softcore_alpha*(1.0-lambda_sterics))^(1/coeff_a);"
+    )
+    wca_repulsive.addGlobalParameter("lambda_sterics", lambda_value)
+    wca_repulsive.addGlobalParameter("softcore_alpha", softcore_alpha)
+    wca_repulsive.addGlobalParameter("R_particle", particle_radius)
+    wca_repulsive.addGlobalParameter("coeff_r", coeff_r)
+    wca_repulsive.addGlobalParameter("coeff_a", coeff_a)
+    wca_repulsive.addGlobalParameter("sigma_wall", wca_sigma)
+    wca_repulsive.addGlobalParameter("epsilon_wall", wca_epsilon)
+    wca_repulsive.addPerParticleParameter("sigma")
+    wca_repulsive.addPerParticleParameter("epsilon")
+    wca_repulsive.setNonbondedMethod(openmm.CustomNonbondedForce.CutoffPeriodic)
+    wca_repulsive.setCutoffDistance(nonbonded.getCutoffDistance())
+    wca_repulsive.setUseLongRangeCorrection(False)
+    wca_repulsive.setForceGroup(force_group)
+
+    wca_repulsive.addInteractionGroup(guest, solvents)
+
+    # Set LJ parameters
+    for atom in range(nonbonded.getNumParticles()):
+        charge, sigma, epsilon = nonbonded.getParticleParameters(atom)
+        if atom in guest:
+            wca_repulsive.addParticle([wca_sigma, wca_epsilon])
+        else:
+            wca_repulsive.addParticle([sigma, epsilon])
+
+    # Set LJ parameters to zero
+    for atom in guest:
+        [charge, sigma, epsilon] = nonbonded.getParticleParameters(atom)
+        nonbonded.setParticleParameters(atom, 0.0, wca_sigma, 0.0)
+
+    # Transfer Exclusion
+    for exception_index in range(nonbonded.getNumExceptions()):
+        iatom, jatom, chargeprod, sigma, epsilon = nonbonded.getExceptionParameters(
+            exception_index
+        )
+        wca_repulsive.addExclusion(iatom, jatom)
+
+    # Add Force to System
+    system.addForce(wca_repulsive)
